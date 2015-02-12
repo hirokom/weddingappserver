98159c831cf0028483d573d310d4fb37636f719a
司 <@home-PC>
2015-02-12 14:28:02 +0900

    tuthiya

--- weddingappserver/src/main/java/com/abeam/weddingappserver/domain/repository/UserRepository.java
+++ weddingappserver/src/main/java/com/abeam/weddingappserver/domain/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.abeam.weddingappserver.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.abeam.weddingappserver.domain.entity.User;
 import com.sun.xml.internal.bind.v2.model.core.ID;
 
 public interface UserRepository extends JpaRepository<User, ID> {
 
 
-	public User findByUserAndId(String User, String ID)
+	public User findByUserAndId(String User, String ID);
 
 }
