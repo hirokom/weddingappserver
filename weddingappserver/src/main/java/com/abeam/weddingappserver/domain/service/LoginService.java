f99d943d7026912445a689a24f0db63ee75b5e06
hirokom <nanohana_9@hotmail.co.jp>
2015-02-12 16:48:15 +0900

    LoginService

--- weddingappserver/src/main/java/com/abeam/weddingappserver/domain/service/LoginService.java
+++ weddingappserver/src/main/java/com/abeam/weddingappserver/domain/service/LoginService.java
@@ -1,9 +1,23 @@
 package com.abeam.weddingappserver.domain.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.abeam.weddingappserver.web.form.WeddingForm;
+import com.abeam.weddingappserver.domain.entity.Couple;
+import com.abeam.weddingappserver.domain.entity.FoodMenu;
+import com.abeam.weddingappserver.domain.entity.Hall;
+import com.abeam.weddingappserver.domain.entity.Participant;
+import com.abeam.weddingappserver.domain.entity.User;
+import com.abeam.weddingappserver.domain.entity.Wedding;
+import com.abeam.weddingappserver.domain.model.WeddingInfo;
+import com.abeam.weddingappserver.domain.repository.CoupleRepository;
+import com.abeam.weddingappserver.domain.repository.FoodMenuRepository;
+import com.abeam.weddingappserver.domain.repository.HallRepository;
+import com.abeam.weddingappserver.domain.repository.ParticipantRepository;
+import com.abeam.weddingappserver.domain.repository.UserRepository;
+import com.abeam.weddingappserver.domain.repository.WeddingRepository;
 
 
 @Service
@@ -11,22 +25,45 @@ public class LoginService
 {
 
 	@Autowired
-	UserRepository UserRepository
+	UserRepository userRepository;
 
-	public String loginCheck(final String useid, final String password)
+	@Autowired
+	CoupleRepository coupleRepository;
+
+	@Autowired
+	WeddingRepository weddingRepository;
+
+	@Autowired
+	FoodMenuRepository foodMenuRepository;
+
+	@Autowired
+	HallRepository hallRepository;
+
+	@Autowired
+	ParticipantRepository participantRepository;
+
+	public WeddingInfo getWeddingInfo(final String useid, final String password)
 	{
+		final WeddingInfo weddingInfo = new WeddingInfo();
 
-		final String weddingId = null;
-		return weddingId;
+		User user = userRepository.findByUserAndId(useid, password);
 
-	}
+		List<Couple> coupleList = coupleRepository.findByWeddingId(user.getWeddingId());
+		Wedding wedding = weddingRepository.findByWeddingId(user.getUserId());
+		Hall hall = hallRepository.findByhallId(wedding.getHallId());
+		FoodMenu foodMenu = foodMenuRepository.findByFoodMenuId(wedding.getFoodMenueId());
+		List<Participant> participantList = participantRepository.findByWeddingId(user.getWeddingId());
 
-	public  getweddingForm(final String weddingid)
-	{
+		weddingInfo.setCoupleList(coupleList);
+		weddingInfo.setWedding(wedding);
+		weddingInfo.setHall(hall);
+		weddingInfo.setFoodMenu(foodMenu);
+		weddingInfo.setParticipantList(participantList);
 
-		final WeddingForm weddingForm = new WeddingForm();
-		return weddingForm;
+		return weddingInfo;
 
 	}
 
 }
+
+
